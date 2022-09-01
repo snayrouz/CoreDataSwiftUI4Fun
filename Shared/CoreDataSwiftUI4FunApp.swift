d1c87a045b30042d382ad042b124e18aa4d360aa
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-09-01 13:03:16 -0600

    Initial Commit

---
+++ Shared/CoreDataSwiftUI4FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSwiftUI4FunApp.swift
+//  Shared
+//
+//  Created by Nayrouz, Samuel on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSwiftUI4FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
